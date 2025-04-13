19fba7c1152ea8cac46f93d1795e79ce5b17b23e
Jatin Madaan <jatinmadaan9882@gmail.com>
2025-04-13 18:40:15 +0530

    Refactor user routes to include account management and media upload endpoints

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,16 @@
 import { Router } from 'express';
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from '../controllers/user.controller.js';
+import { 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    getUserChannelProfile,
+    getWatchHistory, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    registerUser, 
+    updateAccountDetails, 
+    updateAvatar, 
+    updateCoverImage } from '../controllers/user.controller.js';
 import upload from '../middlewares/multer.middleware.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 
@@ -25,8 +36,35 @@ router.route('/register').post(
 router.route('/login').post(loginUser);
 
 // secured routes
-router.route('/logout').post(verifyJWT ,logoutUser);
+router.route('/logout').post(verifyJWT, logoutUser);
 router.route('/refresh-token').post(refreshAccessToken);
 
+router.route('/change-password').post(verifyJWT, changeCurrentPassword);
+router.route('/current-user').get(verifyJWT, getCurrentUser);
+router.route('/update-account').patch(verifyJWT, updateAccountDetails);
+router.route('/avatar').patch(verifyJWT, (req, res, next) => {
+    upload.single("avatar")(req, res, (err) => {
+        if (err) {
+            return res.status(400).json({
+                error: err.message
+            });
+        }
+        next();
+    });
+}, updateAvatar);
+
+router.route('/cover-image').patch(verifyJWT, (req, res, next) => {
+    upload.single("coverImage")(req, res, (err) => {
+        if (err) {
+            return res.status(400).json({
+                error: err.message
+            });
+        }
+        next();
+    });
+}, updateCoverImage);
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
